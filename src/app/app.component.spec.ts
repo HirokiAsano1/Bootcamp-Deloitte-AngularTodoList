e8bed72e6dcdbc05ff6f13a50bc89d15fd9f1100
hirokiasano1 <hirokiasanovieira@gmail.com>
2025-05-20 18:08:57 -0300

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,11 +14,6 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'Angular-TODO' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular-TODO');
-  });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
